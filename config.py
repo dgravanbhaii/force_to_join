a2e134ae2bb11fa247effaf45f110b6a3c3f4016
【ＤＧ】ʀᴀᴠᴀɴ 🜲 <dgravanbhai@gmail.com>
2026-08-09 08:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN is missing in .env")
+
+if not OWNER_ID:
+    raise ValueError("OWNER_ID is missing in .env")
